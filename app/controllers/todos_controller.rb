20d83a0721bf2681aa60220c5c209effc0fb6f79
Alex Chalk <alexchalk17@gmail.com>
2017-07-28 10:17:54 -0400

    Create getter and setter methods for session[:current_email]
    If there is ever any need to change how a sign_in is implemented, or how
    a current email is loaded, each change can now be made in one place:
    `sign_in_as` or `current_email` in the application controller.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
   before_action :authenticate
   def index
-    @todos = Todo.where(email: session[:current_email])
+    @todos = Todo.where(email: current_email)
   end
 
   def new
@@ -16,6 +16,6 @@ class TodosController < ApplicationController
   private
 
   def todo_params
-    params.require(:todo).permit(:title).merge(email: session[:current_email])
+    params.require(:todo).permit(:title).merge(email: current_email)
   end
 end
